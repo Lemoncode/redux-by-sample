87783ae495d1836543e8281d42c449fce5057c00
nasdan <daniel.sanchez@lemoncode.net>
2016-11-15 10:42:03 +0100

    Configuring jest to work with @types jest d.ts

---
+++ 11 Testing_Jest/src/pages/login/actions/specs/loginRequestCompleted.spec.ts
@@ -0,0 +1,9 @@
+import {actionsEnums} from '../../../../common/actionsEnums';
+import {LoginResponse} from '../../../../model/loginResponse';
+
+describe('loginRequestCompleted', () => {
+  it('', () => {
+    var result = true;
+    expect(result).toBeTruthy();
+  })
+});
