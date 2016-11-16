f29b4014a0b70bb712c950655ac8b90307d831be
nasdan <daniel.sanchez@lemoncode.net>
2016-11-16 18:29:24 +0100

    Testing simple action

--- 11 Testing_Jest/src/pages/login/actions/specs/loginRequestCompleted.spec.ts
+++ 11 Testing_Jest/src/pages/login/actions/specs/loginRequestCompleted.spec.ts
@@ -1,10 +1,64 @@
 // @types global declarations https://www.typescriptlang.org/docs/handbook/tsconfig-json.html#types-typeroots-and-types
 import {actionsEnums} from '../../../../common/actionsEnums';
 import {LoginResponse} from '../../../../model/loginResponse';
+import {UserProfile} from '../../../../model/userProfile'
+import {loginRequestCompletedAction} from '../loginRequestCompleted';
 
 describe('loginRequestCompleted', () => {
-  it('', () => {
-    var result = true;
-    expect(result).toBeTruthy();
+  describe('#loginRequestCompletedAction', () => {
+    it('should return action { type: USERPROFILE_PERFORM_LOGIN, payload: undefined } ' +
+    'when passing loginResponse equals undefined', () => {
+      //Arrange
+      let loginResponse = undefined;
+
+      //Act
+      var result = loginRequestCompletedAction(loginResponse);
+
+      //Assert
+      expect(result.type).toBe(actionsEnums.USERPROFILE_PERFORM_LOGIN);
+      expect(result.payload).toBeUndefined();
+    });
+
+    it('should return action { type: USERPROFILE_PERFORM_LOGIN, payload: null } ' +
+    'when passing loginResponse equals null', () => {
+      //Arrange
+      let loginResponse = null;
+
+      //Act
+      var result = loginRequestCompletedAction(loginResponse);
+
+      //Assert
+      expect(result.type).toBe(actionsEnums.USERPROFILE_PERFORM_LOGIN);
+      expect(result.payload).toBeNull();
+    });
+
+    it('should return action { type: USERPROFILE_PERFORM_LOGIN, payload: {succeeded: true} } ' +
+    'when passing loginResponse equals {succeeded: true}', () => {
+      //Arrange
+      let loginResponse = new LoginResponse();
+      loginResponse.succeeded = true;
+
+      //Act
+      var result = loginRequestCompletedAction(loginResponse);
+
+      //Assert
+      expect(result.type).toBe(actionsEnums.USERPROFILE_PERFORM_LOGIN);
+      expect(result.payload.succeeded).toBeTruthy();
+    });
+
+    it('should return action { type: USERPROFILE_PERFORM_LOGIN, payload: {userProfile: {fullname: "test"}} } ' +
+    'when passing loginResponse equals {userProfile: {fullname: "test"}}', () => {
+      //Arrange
+      let loginResponse = new LoginResponse();
+      loginResponse.userProfile = new UserProfile();
+      loginResponse.userProfile.fullname = "test";
+
+      //Act
+      var result = loginRequestCompletedAction(loginResponse);
+
+      //Assert
+      expect(result.type).toBe(actionsEnums.USERPROFILE_PERFORM_LOGIN);
+      expect(result.payload.userProfile.fullname).toEqual("test");
+    });
   })
 });
