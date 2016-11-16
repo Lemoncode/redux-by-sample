e399cadf919827b1e5db5eb6752ab02b9162f823
Braulio <braulio.diez@gmail.com>
2016-11-16 12:14:47 +0100

    Students reducer spec completed

--- 13 Testing Reducer/src/reducers/spec/student.spec.ts
+++ 13 Testing Reducer/src/reducers/spec/student.spec.ts
@@ -5,7 +5,8 @@ import {studentReducer} from '../student';
 import {} from 'mocha'
 import {StudentEntity} from '../../model/student'
 import {StudentErrors} from '../../model/studentErrors'
-
+import {IStudentFieldValueChangedCompletedPayload} from '../../pages/student-detail/actions/studentFieldValueChangedCompleted'
+import {FieldValidationResult} from 'lc-form-validation'
 
 describe('studentReducer', () => {
   describe('#handleGetStudentList', () => {
@@ -104,6 +105,145 @@ describe('studentReducer', () => {
       expect(finalState.studentsList).to.be.null;
     });
 
+
+  });
+
+  describe('#handleGetStudent', () => {
+    it('Store in reducer properly built student', () => {
+      // Arrange
+      const initialState = {
+        studentsList : [],
+        editingStudent : new StudentEntity(),
+        editingStudentErrors : new StudentErrors()
+      };
+
+      const student = new StudentEntity();
+      student.id = 1;
+      student.gotActiveTraining = false;
+      student.fullname = "john";
+      student.email = "john@email.com"
+
+      const action = {
+        type: actionsEnums.STUDENT_GET_STUDENT_REQUEST_COMPLETED,
+        payload: student
+      };
+
+      // Act
+      const finalState = studentReducer(initialState, action)
+
+      // Assert
+      expect(finalState.editingStudent.id).to.be.equal(student.id );
+      expect(finalState.editingStudent.gotActiveTraining).to.be.equal(student.gotActiveTraining);
+      expect(finalState.editingStudent.fullname).to.be.equal(student.fullname);
+      expect(finalState.editingStudent.email).to.be.equal(student.email);
+    });
+
+    it('Store in reducer null student', () => {
+      // Arrange
+      const initialState = {
+        studentsList : [],
+        editingStudent : new StudentEntity(),
+        editingStudentErrors : new StudentErrors()
+      };
+
+      const student = null;
+
+      const action = {
+        type: actionsEnums.STUDENT_GET_STUDENT_REQUEST_COMPLETED,
+        payload: student
+      };
+
+      // Act
+      const finalState = studentReducer(initialState, action)
+
+      // Assert
+      expect(finalState.editingStudent).to.be.null;
+    });
+
+  });
+
+  describe('#handleFieldValueChanged', () => {
+    it('given an existing student in state update fullname no validation errors', () => {
+      // Arrange
+      const studentA = new StudentEntity();
+
+      studentA.id = 1;
+      studentA.fullname = "john";
+
+      const initialState = {
+                            studentsList : [],
+                            editingStudent : studentA,
+                            editingStudentErrors : new StudentErrors()
+                           };
+
+      deepFreeze(initialState);
+
+      const newFullnameValue = 'johns';
+      const fieldValidationResult = new FieldValidationResult();
+      fieldValidationResult.key = 'fullname';
+      fieldValidationResult.succeeded = true;
+
+      const payload : IStudentFieldValueChangedCompletedPayload = {
+         fieldName: 'fullname',
+         value : newFullnameValue,
+         fieldValidationResult : fieldValidationResult
+      };
+
+      const action = {
+        type: actionsEnums.STUDENT_FIELD_VALUE_CHANGED_COMPLETED,
+        payload: payload
+      };
+
+      // Act
+      const finalState = studentReducer(initialState, action);
+
+      // Assert
+      expect(finalState.editingStudent.fullname).to.be.equal(newFullnameValue);
+      expect(finalState.editingStudentErrors.fullname.succeeded).to.be.true;
+    });
+
+    it('given an existing student in state update fullname with validation errors', () => {
+      // Arrange
+      const studentA = new StudentEntity();
+
+      studentA.id = 1;
+      studentA.fullname = "john";
+
+      const initialState = {
+                            studentsList : [],
+                            editingStudent : studentA,
+                            editingStudentErrors : new StudentErrors()
+                           };
+
+      deepFreeze(initialState);
+
+      const newFullnameValue = '';
+      const fieldValidationResult = new FieldValidationResult();
+      fieldValidationResult.key = 'fullname';
+      fieldValidationResult.succeeded = false;
+      fieldValidationResult.type = 'REQUIRED';
+      fieldValidationResult.errorMessage = 'Required field';
+
+      const payload : IStudentFieldValueChangedCompletedPayload = {
+         fieldName: 'fullname',
+         value : newFullnameValue,
+         fieldValidationResult : fieldValidationResult
+      };
+
+      const action = {
+        type: actionsEnums.STUDENT_FIELD_VALUE_CHANGED_COMPLETED,
+        payload: payload
+      };
+
+      // Act
+      const finalState = studentReducer(initialState, action);
+
+      // Assert
+      expect(finalState.editingStudent.fullname).to.be.equal(newFullnameValue);
+      expect(finalState.editingStudentErrors.fullname.succeeded).to.be.false;
+      expect(finalState.editingStudentErrors.fullname.type).to.be.equal('REQUIRED');
+      expect(finalState.editingStudentErrors.fullname.errorMessage).to.be.equal('Required field');
+    });
 
   });
 });
