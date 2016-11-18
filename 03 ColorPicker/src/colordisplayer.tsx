7af7f3f357cceeffa879938408cb1d66fdaa47cc
linuxonrails <linuxonrails@linuxonrails.com>
2016-11-18 09:42:23 +0100

    Review 03 Colorpicker. This closes Lemoncode/redux-by-sample#4

--- 03 ColorPicker/src/colordisplayer.tsx
+++ 03 ColorPicker/src/colordisplayer.tsx
@@ -1,22 +1,21 @@
-import * as React from 'react';
-import {Color} from './model/color';
+import * as React from "react";
+import { Color } from "./model/color";
 
 interface Props {
-  color : Color;
+  color: Color;
 }
 
-export const ColorDisplayer = (props : Props) => {
+export const ColorDisplayer = (props: Props) => {
   // `rgb(${props.color.red},${props.color.green}, ${props.color.blue}) })`
-  // 'rgb(' + props.color.red + ', 40, 80)'
-  var divStyle = {
-    width: '120px',
-    height: '80px',
+  // "rgb(" + props.color.red + ", 40, 80)"
+  let divStyle = {
+    width: "120px",
+    height: "80px",
     backgroundColor: `rgb(${props.color.red},${props.color.green}, ${props.color.blue})`
   };
-
 
   return (
     <div style={divStyle}>
     </div>
   );
-}
+};
