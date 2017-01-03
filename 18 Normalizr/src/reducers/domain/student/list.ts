3ce890c12d81699d63cb69c26daf73b30eae3376
nasdan <daniel.sanchez@lemoncode.net>
2017-01-03 12:23:27 +0100

    Replace byId switch by if to extisting entities property

--- 18 Normalizr/src/reducers/domain/student/list.ts
+++ 18 Normalizr/src/reducers/domain/student/list.ts
@@ -3,13 +3,12 @@ import { actionsEnums } from "../../../common/actionsEnums";
 import { StudentEntity } from "../../../model/student";
 import { StudentErrors } from "../../../model/studentErrors";
 
-export const byId = (state : {[id : number] : StudentEntity} = {}, action) => {
-  switch (action.type) {
-    case actionsEnums.STUDENTS_GET_LIST_REQUEST_COMPLETED:
-      return {
-        ...state,
-        ...action.payload.entities.students
-      }
+export const byId = (state : {[id: number] : StudentEntity} = {}, action) => {
+  if (action.payload && action.payload.entities) {
+    return {
+      ...state,
+      ...action.payload.entities.students
+    }
   }
   return state;
 }
