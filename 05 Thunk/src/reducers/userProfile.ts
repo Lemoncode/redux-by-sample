2aae1a0d98930fbb40e8dfb1d52b2462cff4ba03
nasdan <daniel.sanchez@lemoncode.net>
2017-04-07 22:43:05 +0200

    Fix sample

--- 05 Thunk/src/reducers/userProfile.ts
+++ 05 Thunk/src/reducers/userProfile.ts
@@ -1,37 +1,39 @@
 import {actionsEnums} from '../common/actionsEnums';
 import {updateUserProfileName} from '../actions/updateUserProfileName';
-import {Color} from '../model/color';
-import objectAssign = require('object-assign');
+import { Color } from "../model/color";
 
-class UserProfileState  {
+class UserProfileState {
   firstname : string;
-  favouriteColor : Color;
+  favouriteColor: Color;
 
-  public constructor()
-  {
+  constructor() {
     this.firstname = "Default name";
     this.favouriteColor = {red: 0, green: 0, blue: 180};
   }
 }
 
 export const userProfileReducer =  (state : UserProfileState = new UserProfileState(), action) => {
-      switch (action.type) {
-        case actionsEnums.UPDATE_USERPROFILE_NAME:
-           return handleUserProfileAction(state, action);
-        case actionsEnums.UPDATE_USERPROFILE_FAVOURITE_COLOR:
-           return handleFavouriteColorAction(state, action);
-      }
+  switch (action.type) {
+    case actionsEnums.UPDATE_USERPROFILE_NAME:
+      return handleUserProfileAction(state, action);
 
-      return state;
+    case actionsEnums.UPDATE_USERPROFILE_FAVOURITE_COLOR:
+      return handleFavouriteColorAction(state, action);
+  }
+
+  return state;
 };
 
-
-const handleFavouriteColorAction = (state : UserProfileState, action) => {
-  const newState = objectAssign({}, state, {favouriteColor: action.newColor});
-  return newState;
-}
-
 const handleUserProfileAction = (state : UserProfileState, action) => {
-  const newState = objectAssign({}, state, {firstname: action.newName});
-  return newState;
+  return {
+    ...state,
+    firstname: action.newName,
+  };
 }
+
+const handleFavouriteColorAction = (state: UserProfileState, action) => {
+  return {
+    ...state,
+    favouriteColor: action.newColor,
+  };
+};
