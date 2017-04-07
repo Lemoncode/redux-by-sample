d5693463c4fec9f735c73db0e86cec8287727cf8
nasdan <daniel.sanchez@lemoncode.net>
2017-04-07 22:44:45 +0200

    Fixing color component

---
+++ 05 Thunk/src/components/color/colorDisplayerContainer.ts
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import { ColorDisplayer } from './colordisplayer';
+
+const mapStateToProps = (state) => {
+  return {
+    color: state.userProfileReducer.favouriteColor
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+  };
+};
+
+export const ColorDisplayerContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ColorDisplayer);
