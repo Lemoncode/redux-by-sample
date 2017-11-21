5b72b597ff4aaa466409050a0932ab41f8ac6f08
Jairo Caro-Accino Viciana <jairo@galax.be>
2017-11-21 12:41:18 +0100

    Update main.tsx
    Prefer const over let if there is no needed of using let.

--- 01 HelloRedux/src/main.tsx
+++ 01 HelloRedux/src/main.tsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import {reducers} from './reducers';
 import {HelloWorldContainer} from './helloWorldContainer';
 
-let store = createStore(reducers);
+const store = createStore(reducers);
 
 ReactDOM.render(
   <Provider store={store}>
