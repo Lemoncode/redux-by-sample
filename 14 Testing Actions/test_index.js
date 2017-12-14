b54a66098e0a11bc82677c870d63d39ab3ab5b75
Santiago Camargo Rodríguez <santi.camargo.rodriguez@gmail.com>
2017-12-14 23:54:23 +0100

    Updated dependencies and fixed issues

--- 14 Testing Actions/test_index.js
+++ 14 Testing Actions/test_index.js
@@ -1,5 +1,12 @@
+// Configure enzyme adapter
+const enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
+enzyme.configure({ adapter: new Adapter() });
+
 // require all modules ending in ".spec" from the
 // current directory and all subdirectories
-
-var testsContext = require.context("./src", true, /.spec$/);
+const testsContext = require.context('./src', true, /.spec$/);
 testsContext.keys().forEach(testsContext);
+
+const componentsContext = require.context('./src', true, /.ts$/);
+componentsContext.keys().forEach(componentsContext);
